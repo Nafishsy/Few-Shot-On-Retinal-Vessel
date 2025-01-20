3d874f9551d4cf5673b0a330c42d18673e6400c7
Nafishsy <nafizahmed273273@gmail.com>
2025-01-20 16:52:20 +0600

    debug on

--- api/index.py
+++ api/index.py
@@ -190,5 +190,5 @@ def predict():
 # Run the app
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))  # Ensure correct port
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
 
